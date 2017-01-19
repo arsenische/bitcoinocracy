2494e1e846b6d5b4cd4fc35bf7bbb4586e99ff0a
Arsen Gasparyan <arsenische@gmail.com>
2017-01-20 02:58:20 +0500

    autohide old arguments without signatures in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :update_bitcoin_balance # since we don't yet have a background task, we need this hack :(
+  before_filter :do_background_jobs # since we don't yet have a background job, we need this hack :(
+
+  def do_background_jobs
+    update_bitcoin_balance if rand < 0.1 # don't slowdown 90% of the requests
+    hide_old_arguments_without_signature
+  end
 
   def update_bitcoin_balance
-    return if rand > 0.9 # don't slowdown 90% of requests
-
     if BitcoinAddress.where('updated_at > ?',10.seconds.ago).empty? # don't DOS blockchain.info
       bitcoin_address = BitcoinAddress.unscoped.order("updated_at ASC").first
       if bitcoin_address.updated_at < 10.minutes.ago # no need to do it too often
         bitcoin_address.update_balance
       end
+    end
+  end
+
+  def hide_old_arguments_without_signature
+    Argument.visible.where('created_at < ? and all_sum = 0').each do |a|
+      a.hide!
     end
   end
 
