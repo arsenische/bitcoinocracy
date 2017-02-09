edc7003653279d30414fa2418ec631f02ce86625
Sam Cheng Hung <samchenghung@gmail.com>
2017-02-09 20:27:12 +0800

    Sidebar changes and some argument display page changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
+// require turbolinks
 //= require_tree .
--- app/assets/javascripts/argument_vote.js
+++ app/assets/javascripts/argument_vote.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   $('#vote-agree').click(function() {
     $('#vote-form').slideDown(300);
+    $(this)
+    .removeClass('inactive')
+    .siblings().addClass('inactive');
   });
 
   $('#vote-doubt').click(function() {
     $('#vote-form').slideDown(300);
+    $(this)
+    .removeClass('inactive')
+    .siblings().addClass('inactive');
   });
 });
