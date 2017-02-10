52a7af72e83abbfe744471aa40d28c45710f23e8
Sam Cheng Hung <samchenghung@gmail.com>
2017-02-10 15:23:42 +0800

    Updates argument page

--- app/assets/javascripts/argument_vote.js
+++ app/assets/javascripts/argument_vote.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
   $('#vote-agree').click(function() {
-    $('#vote-form').slideDown(300);
-    $(this)
-    .removeClass('inactive')
-    .siblings().addClass('inactive');
+    activateButton($(this));
+    updateStatement('agree');
   });
 
   $('#vote-doubt').click(function() {
+    activateButton($(this));
+    updateStatement('doubt');
+  });
+
+  var activateButton = function(button) {
     $('#vote-form').slideDown(300);
-    $(this)
+    button
     .removeClass('inactive')
     .siblings().addClass('inactive');
-  });
+  }
+
+  var updateStatement = function(statement) {
+    var newStatement;
+    switch(statement) {
+      case 'agree':
+        newStatement = $('#pro_statement').val();
+        break;
+      case 'doubt':
+        newStatement = $('#con_statement').val();
+        break;
+      default:
+        newStatement = '';
+    }
+    $('#argument-statement').html(newStatement);
+  }
 });
